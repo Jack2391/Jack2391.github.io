cab87feec42a586b83bc3e8517df405b9a9f740d
Jack2391 <nikosinc@gmail.com>
2017-03-16 14:49:13 +0300

    1

---
+++ log.php
@@ -0,0 +1,21 @@
+﻿<?php
+function get_ip()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+        $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+echo get_ip();
+
+?>
